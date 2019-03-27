ad90f08a56be9b6a371f55b2110d015c25e44881
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-03-26 20:56:58 -0400

    detail views and list views

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse        
-# Create your views here.
-
-posts = [
-    {
-        'author': 'CoreyInDaHouse',
-        'title':'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'JaneBro',
-        'title':'Blog 2',
-        'content': 'First Post Content',
-        'date_posted': 'August 29, 2018'
-    }
-]
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post      
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'posts'
+
+
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About',})
+    return render(request, 'blog/about.html', {'title': 'About'})
